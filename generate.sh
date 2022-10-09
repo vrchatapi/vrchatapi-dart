7ded547fc7ca4d2cfa575d7909814237c854eae6
Rexios <rexios@rexios.dev>
2022-10-09 00:38:37 -0400

    Updated generation script

--- generate.sh
+++ generate.sh
@@ -1,6 +1,10 @@
 # Note: This can take a while to run (about a minute for me)
 
-cd vrchat_dart
+cd vrchat_dart_generated
+# Remove old files
+rm -rf doc lib test
+
+cd ../vrchat_dart
 
 # For some reason the openapi generator won't run without a clean
 flutter clean
