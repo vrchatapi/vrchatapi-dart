339172766f53d48598401baa2406d96507f44880
Rexios <rexios@rexios.dev>
2022-12-05 09:48:08 -0500

    Override enum toString for query parameters

--- vrchat_dart/tool/patch_output.dart
+++ vrchat_dart/tool/patch_output.dart
@@ -6,6 +6,11 @@ void main() async {
   print('Model patched!');
 }
 
+final _enumToString = r'''
+  @override
+  String toString() => this.name;
+''';
+
 Future<void> patchModel() async {
   final modelFiles = Directory('../vrchat_dart_generated/lib/src/model')
       .listSync()
@@ -19,6 +24,11 @@ Future<void> patchModel() async {
         .replaceAllMapped(
           RegExp(r"const (.*)Enum\._\('(.*)'\)"),
           (match) => '${match.group(1)}Enum.${match.group(2)}',
+        )
+        .replaceAllMapped(
+          RegExp(r'enum (.*) {((.|\n)*),\n}'),
+          (match) =>
+              'enum ${match.group(1)} {${match.group(2)};\n$_enumToString}',
         );
 
     file.writeAsStringSync(content);
