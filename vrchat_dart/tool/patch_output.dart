d223ef3ab2c1c17ebb0af2aeb6b929c19bff2217
Rexios <rexios@rexios.dev>
2023-02-19 13:21:53 -0500

    Working on patching issues

--- vrchat_dart/tool/patch_output.dart
+++ vrchat_dart/tool/patch_output.dart
@@ -1,9 +1,16 @@
 import 'dart:io';
 
+import 'package:collection/collection.dart';
+import 'package:parselyzer/parselyzer.dart';
+
 void main() async {
   print('Patching lib/src/model...');
   await patchModel();
   print('Model patched!');
+
+  print('Patching analysis issues...');
+  await patchAnalysisIssues();
+  print('Analysis issues patched!');
 }
 
 final _enumToString = r'''
@@ -32,5 +39,37 @@ Future<void> patchModel() async {
         );
 
     file.writeAsStringSync(content);
+  }
+}
+
+Future<void> patchAnalysisIssues() async {
+  final result = await Process.run(
+    'dart',
+    ['analyze', '--format=json'],
+    workingDirectory: '../vrchat_dart_generated',
+  );
+
+  final analysis = AnalyzerResult.fromConsole(result.stdout as String);
+  if (analysis == null) return;
+
+  final deprecationIssues = analysis.diagnostics
+      .where((e) => e.code == 'deprecated_member_use_from_same_package')
+      .groupListsBy((e) => e.location.file);
+  for (final entry in deprecationIssues.entries) {
+    final file = File(entry.key);
+    final lineNumbers = entry.value.map((e) => e.location.range.start.line);
+
+    final lines = file.readAsLinesSync();
+    final newContents = lines.mapIndexed((index, line) {
+      // Indices in [lineNumbers] are 1 indexed
+      // Indices in [lines] are 0 indexed
+      if (lineNumbers.contains(index + 1)) {
+        return '// ignore: deprecated_member_use_from_same_package\n$line';
+      } else {
+        return line;
+      }
+    }).join('\n');
+
+    file.writeAsStringSync(newContents);
   }
 }
