47e7efa069c8d8be76a0a94478f7b19a42f083f0
Rexios <rexios@rexios.dev>
2025-06-22 22:15:32 -0400

    Fixing conversion issues

--- vrchat_dart/lib/src/convenience/user_extensions.dart
+++ vrchat_dart/lib/src/convenience/user_extensions.dart
@@ -4,15 +4,12 @@ import 'package:vrchat_dart/vrchat_dart.dart';
 extension CurrentUserExtension on CurrentUser {
   /// Convert a [CurrentUser] to a [User]
   User toUser() {
-    final currentUserJson = toJson();
-    currentUserJson['last_activity'] = '';
-    return User.fromJson(currentUserJson);
+    return User.fromJson(toJson());
   }
 
   /// Convert a [CurrentUser] to a [LimitedUser]
   LimitedUser toLimitedUser() {
-    final currentUserJson = toJson();
-    return LimitedUser.fromJson(currentUserJson);
+    return LimitedUser.fromJson(toJson());
   }
 }
 
@@ -20,10 +17,11 @@ extension CurrentUserExtension on CurrentUser {
 extension UserExtension on User {
   /// Convert a [User] to a [LimitedUser]
   LimitedUser toLimitedUser() {
-    final userJson = toJson();
-    userJson['last_login'] =
-        lastLogin.isNotEmpty ? DateTime.parse(lastLogin) : null;
-    return LimitedUser.fromJson(userJson);
+    return LimitedUser.fromJson({
+      ...toJson(),
+      'last_login': DateTime.tryParse(lastLogin),
+      'last_activity': DateTime.tryParse(lastActivity),
+    });
   }
 }
 
@@ -31,8 +29,7 @@ extension UserExtension on User {
 extension LimitedUserInstanceExtension on LimitedUserInstance {
   /// Convert a [LimitedUserInstance] to a [LimitedUser]
   LimitedUser toLimitedUser() {
-    final limitedUserInstanceJson = toJson();
-    return LimitedUser.fromJson(limitedUserInstanceJson);
+    return LimitedUser.fromJson(toJson());
   }
 }
 
@@ -40,8 +37,7 @@ extension LimitedUserInstanceExtension on LimitedUserInstance {
 extension LimitedUserFriendExtension on LimitedUserFriend {
   /// Convert a [LimitedUserFriend] to a [LimitedUser]
   LimitedUser toLimitedUser() {
-    final limitedUserFriendJson = toJson();
-    return LimitedUser.fromJson(limitedUserFriendJson);
+    return LimitedUser.fromJson(toJson());
   }
 }
 
@@ -49,7 +45,6 @@ extension LimitedUserFriendExtension on LimitedUserFriend {
 extension LimitedUserSearchExtension on LimitedUserSearch {
   /// Convert a [LimitedUserSearch] to a [LimitedUser]
   LimitedUser toLimitedUser() {
-    final limitedUserSearchJson = toJson();
-    return LimitedUser.fromJson(limitedUserSearchJson);
+    return LimitedUser.fromJson(toJson());
   }
 }
